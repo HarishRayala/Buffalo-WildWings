5aced346d6b648caf942a7604cb02a2077245bd7
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-03-01 14:37:40 +0530

    completed menu

--- Buffalo_WIldWings/index.js
+++ Buffalo_WIldWings/index.js
@@ -1,18 +1,36 @@
-var owl = document.querySelector('.owl-carousel');
-  var options = {
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive:{
-      0:{
-        items:1
-      },
-      600:{
-        items:3
-      },
-      1000:{
-        items:5
-      }
-    }
-  };
-  var carousel = new OwlCarousel(owl, options);
+// ------------Menu-------------
+import { menu } from "./Data/data.js";
+console.log(menu);
+let menuSection = document.querySelector(".main_menu");
+
+for (let i = menu.length - 1; i >= 0; i--) {
+  console.log(menu[i].picture);
+  console.log(menu[i].name);
+  let menuItem = document.createElement("div");
+  menuItem.setAttribute("class", "menuItem");
+  let menuItemImg = document.createElement("img");
+  menuItemImg.src = menu[i].picture;
+  let menuItemName = document.createElement("span");
+  menuItemName.textContent = menu[i].name;
+  menuItem.append(menuItemImg, menuItemName);
+  menuSection.prepend(menuItem);
+}
+
+var owl = document.querySelector(".owl-carousel");
+var options = {
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+};
+var carousel = new OwlCarousel(owl, options);
